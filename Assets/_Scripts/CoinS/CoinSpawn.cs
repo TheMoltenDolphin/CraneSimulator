ab5daa9704d87fc09de48702241b559cc6a82539
Sokotukhin Ivan <ivansokotuhin1816@gmail.com>
2023-08-11 22:37:39 +0700

    Edited COIN MECHANIC

--- Assets/_Scripts/CoinS/CoinSpawn.cs
+++ Assets/_Scripts/CoinS/CoinSpawn.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class CoinSpawn : MonoBehaviour
 {
@@ -10,31 +12,47 @@ public class CoinSpawn : MonoBehaviour
     public GameObject CoinPrefab;
     [SerializeField] private int CoinAmount;
     public int CoinCounter;
+    private float time;
+    private bool IsStarted;
+    [SerializeField] private TextMeshProUGUI TimerText;
+    [SerializeField] private TextMeshProUGUI CollectedCoinCounter;
 
     private void Awake()
     {
         singleton = this;
     }
-    [ContextMenu("Place Coins!")]
-    public void ReplaceCoins()
+    #region FirstMethod
+    //[ContextMenu("Place Coins!")]
+    //public void ReplaceCoins()
+    //{
+    //    int[] excludes = new int[CoinAmount];
+    //    for(int j = 0; j < CoinAmount; j++)
+    //    {
+    //        excludes[j] = 993;
+    //    }
+    //    for (int i = 0; i < CoinAmount; i++)
+    //    {
+    //        int s = RandomFromRangeWithExceptions(0, CoinSpawners.Length+1, excludes);
+    //        Instantiate(CoinPrefab, CoinSpawners[s]);
+    //        excludes[i] = s;
+    //    }
+    //}
+    //int RandomFromRangeWithExceptions(int rangeMin, int rangeMax, params int[] exclude)//exclude -- список чисел которые Ќ≈ должны входить в результат
+    //{
+    //    var _rand = new System.Random();
+    //    var range = Enumerable.Range(rangeMin, rangeMax).Where(i => !exclude.Contains(i));//создаем  колекцию допустимых значений
+    //    int index = Random.Range(rangeMin, rangeMax - exclude.Length);//генерируем индекс €чейки
+    //    return range.ElementAt(index);//возвращаем значение €чейки
+    //}
+    #endregion
+
+    private void FixedUpdate()
     {
-        int[] excludes = new int[CoinAmount];
-        for(int j = 0; j < CoinAmount; j++)
+        if (IsStarted)
         {
-            excludes[j] = 993;
+            time += Time.fixedDeltaTime;
+            TimerText.text = $"ѕрошло\n {time.ToString()} секунд ";
+            CollectedCoinCounter.text = CoinCounter.ToString();
         }
-        for (int i = 0; i < CoinAmount; i++)
-        {
-            int s = RandomFromRangeWithExceptions(0, CoinSpawners.Length+1, excludes);
-            Instantiate(CoinPrefab, CoinSpawners[s]);
-            excludes[i] = s;
-        }
-    }
-    int RandomFromRangeWithExceptions(int rangeMin, int rangeMax, params int[] exclude)//exclude -- список чисел которые Ќ≈ должны входить в результат
-    {
-        var _rand = new System.Random();
-        var range = Enumerable.Range(rangeMin, rangeMax).Where(i => !exclude.Contains(i));//создаем  колекцию допустимых значений
-        int index = Random.Range(rangeMin, rangeMax - exclude.Length);//генерируем индекс €чейки
-        return range.ElementAt(index);//возвращаем значение €чейки
     }
 }
